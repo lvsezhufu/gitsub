ef2afd8977192dd6908e5c98abfc49ae66b559de
marinalk <liukuai07@126.com>
2017-01-07 15:07:52 +0800

    2

--- sub/sub/Program.cs
+++ sub/sub/Program.cs
@@ -11,7 +11,7 @@ namespace sub
         static void Main(string[] args)
         {
             Console.WriteLine("sub1masterb1");
-            Console.WriteLine("b12");
+            Console.WriteLine("b12a2");
         }
     }
 }
